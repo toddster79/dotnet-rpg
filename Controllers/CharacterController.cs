e95927c42530c93ed4514d677ed03079ee38c86f
Todd Carlson <todd.carlson3030@me.com>
2021-08-12 10:47:47 -0600

    Created and tested Character controller

--- Controllers/CharacterController.cs
+++ Controllers/CharacterController.cs
@@ -1,17 +1,23 @@
 using Microsoft.AspNetCore.Mvc;
 using dotnet_rpg.Models;
+using System.Collections.Generic;
 
 namespace dotnet_rpg.Controllers
 {
     [ApiController]
-    [Route("[controller")]
+    [Route("[controller]")]
     public class CharacterController : ControllerBase
     {
-        private static Character knight = new Character();
-
-        public IActionResult Get()
+        private static List<Character> characters = new List<Character>
         {
-            return Ok(knight);
+            new Character(),
+            new Character { Name = "Gandalf" }
+        };
+
+        [HttpGet]
+        public ActionResult<List<Character>> Get()
+        {
+            return Ok(characters);
         }
 
     }
--- Models/Character.cs
+++ Models/Character.cs
@@ -3,7 +3,7 @@ namespace dotnet_rpg.Models
     public class Character
     {
         public int Id { get; set; }
-        public string Name { get; set; } ="Frod";
+        public string Name { get; set; } ="Frodo";
         public int HitPoints { get; set; } = 100;
         public int Strength { get; set; } = 10;
         public int Defense { get; set; } = 10;
