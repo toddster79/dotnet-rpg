73266113d9169df501e33095e334b6fc87f7c798
Todd Carlson <todd.carlson3030@me.com>
2021-08-12 14:22:00 -0600

    Created service response and updated appropriate methods

--- Controllers/CharacterController.cs
+++ Controllers/CharacterController.cs
@@ -3,6 +3,7 @@ using dotnet_rpg.Models;
 using System.Collections.Generic;
 using System.Linq;
 using dotnet_rpg.Services.CharacterService;
+using System.Threading.Tasks;
 
 namespace dotnet_rpg.Controllers
 {
@@ -19,22 +20,22 @@ namespace dotnet_rpg.Controllers
         }
 
         [HttpGet("GetAll")]
-        public ActionResult<List<Character>> Get()
+        public async Task<ActionResult<List<Character>>> Get()
         {
-            return Ok(_characterService.GetAllCharacters());
+            return Ok(await _characterService.GetAllCharacters());
         }
 
         [HttpGet("{id}")]
-        public ActionResult<Character> GetSingle(int id)
+        public async Task<ActionResult<Character>> GetSingle(int id)
         {
-            return Ok(_characterService.GetCharacterById(id));
+            return Ok(await _characterService.GetCharacterById(id));
         }
 
         [HttpPost]
-        public ActionResult<List<Character>> AddCharacter(Character newCharacter)
+        public async Task<ActionResult<List<Character>>> AddCharacter(Character newCharacter)
         {
             
-            return Ok(_characterService.AddCharacter(newCharacter));
+            return Ok(await _characterService.AddCharacter(newCharacter));
         }
 
     }
---
+++ Models/ServiceResponse.cs
@@ -0,0 +1,13 @@
+namespace dotnet_rpg.Models
+{
+    public class ServiceResponse<T>
+    {
+        public T Data { get; set; }
+
+        public bool Success { get; set; } = true;
+
+        public string Message { get; set; } = null;
+
+
+    }
+}
