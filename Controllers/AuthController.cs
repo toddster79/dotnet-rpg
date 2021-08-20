6e1cc4b1ee5e843b3bd3d54df13e68d15f4d192f
Todd Carlson <todd.carlson3030@me.com>
2021-08-20 13:47:36 -0600

    Created register and tested register method in auth controller.

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,35 @@
+using System.Threading.Tasks;
+using dotnet_rpg.Data;
+using dotnet_rpg.Dtos.User;
+using dotnet_rpg.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace dotnet_rpg.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthRepository _authReop;
+
+        public AuthController(IAuthRepository authReop)
+        {
+            _authReop = authReop;
+        }
+
+        [HttpPost("Register")]
+        public async Task<ActionResult<ServiceResponse<int>>> Register(UserRegisterDto request)
+        {
+            var response = await _authReop.Register(
+                new User { Username = request.Username }, request.Password
+            );
+
+            if(!response.Success)
+            {
+                return BadRequest(response);
+            }
+
+            return Ok(response);
+        }
+    }
+}
